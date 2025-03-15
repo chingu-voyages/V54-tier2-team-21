3ef0e21eca615e87d892ea3bf4d94e39b2174688
Mark <mark.obrien78@btinternet.com>
2025-03-15 14:14:23 +0000

    Add typescript types to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,33 @@
 import { useState } from 'react';
-import './App.css'
+import './App.css';
 // import Header from './components/Header'
-import Form from './components/Form'
-import Prompt from './components/Prompt'
+import Form from './components/Form';
+import Prompt from './components/Prompt';
 // import Footer from './components/Footer'
+import { Inputs } from './types';
 
 function App() {
-  
-  const [formData, setFormData] = useState({}) 
+    const [formData, setFormData] = useState<Inputs>({
+        persona: '',
+        context: '',
+        task: '',
+        output: '',
+        constraint: '',
+    });
 
-  function onFormSubmit(formData) {
-    // console.log(formData);
-    setFormData(formData);
-  }
+    function onFormSubmit(formData: Inputs) {
+        // console.log(formData);
+        setFormData(formData);
+    }
 
-  return (
-    <>
-      {/* <Header /> */}
-      <Form onFormSubmit={onFormSubmit}/>
-      <Prompt formData={formData} />
-      {/* <Footer /> */}
-    </>
-  )
+    return (
+        <>
+            {/* <Header /> */}
+            <Form onFormSubmit={onFormSubmit} />
+            <Prompt formData={formData} />
+            {/* <Footer /> */}
+        </>
+    );
 }
 
 export default App;
