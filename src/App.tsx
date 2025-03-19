ff9c2c8f6c32e6fdcb5610bc4cbd9becdf1e0307
Mark <mark.obrien78@btinternet.com>
2025-03-19 18:02:20 +0000

    Auto changes when saved with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
 import { useState } from 'react';
-import './App.css'
+import './App.css';
 // import Header from './components/Header'
-import Form from './components/Form'
-import Prompt from './components/Prompt'
+import Form from './components/Form';
+import Prompt from './components/Prompt';
 // import Footer from './components/Footer'
 
 function App() {
-  
-  const [formData, setFormData] = useState({}) 
+    const [formData, setFormData] = useState({});
 
-  function onFormSubmit(formData) {
-    // console.log(formData);
-    setFormData(formData);
-  }
+    function onFormSubmit(formData) {
+        // console.log(formData);
+        setFormData(formData);
+    }
 
-  return (
-    <>
-      {/* <Header /> */}
-      <Form onFormSubmit={onFormSubmit}/>
-      <Prompt formData={formData} />
-      {/* <Footer /> */}
-    </>
-  )
+    return (
+        <>
+            {/* <Header /> */}
+            <Form onFormSubmit={onFormSubmit} />
+            <Prompt formData={formData} />
+            {/* <Footer /> */}
+        </>
+    );
 }
 
 export default App;
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,37 +1,55 @@
-import { Button, TextField } from "@mui/material";
-import { useForm, SubmitHandler } from "react-hook-form"
-import { Inputs } from "../types";
+import { Button, TextField } from '@mui/material';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { Inputs } from '../types';
 
-const Form = ({onFormSubmit}) => {
-
-    const {
-        register,
-        handleSubmit,
-      } = useForm<Inputs>();
+const Form = ({ onFormSubmit }) => {
+    const { register, handleSubmit } = useForm<Inputs>();
     //   watch,
     //   formState: { errors },
     const onSubmit: SubmitHandler<Inputs> = (data) => onFormSubmit(data);
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-          <div>
-           <TextField id="persona" label="Persona" variant="outlined" {...register("persona")} sx={{ m: 2 }}/>
-          </div>
-          <div>
-            <TextField id="context" label="Context" variant="outlined"  {...register("context")} sx={{ m: 2 }}/>
-          </div>
-          <div>
-            <TextField id="task" label="Task" variant="outlined"  {...register("task")} sx={{ m: 2 }}/>
-          </div>
-          <div>
-            <TextField id="output" label="Output" variant="outlined"  {...register("output")} sx={{ m: 2 }}/>
-          </div>
-          <div>
-            <TextField id="constraint" label="Constraint" variant="outlined"  {...register("constraint")} sx={{ m: 2 }}/>
-          </div>
-          <Button variant="contained" type="submit">Generate</Button>
+            <TextField
+                id="persona"
+                label="Persona"
+                variant="outlined"
+                {...register('persona')}
+                sx={{ m: 2 }}
+            />
+            <TextField
+                id="context"
+                label="Context"
+                variant="outlined"
+                {...register('context')}
+                sx={{ m: 2 }}
+            />
+            <TextField
+                id="task"
+                label="Task"
+                variant="outlined"
+                {...register('task')}
+                sx={{ m: 2 }}
+            />
+            <TextField
+                id="output"
+                label="Output"
+                variant="outlined"
+                {...register('output')}
+                sx={{ m: 2 }}
+            />
+            <TextField
+                id="constraint"
+                label="Constraint"
+                variant="outlined"
+                {...register('constraint')}
+                sx={{ m: 2 }}
+            />
+            <Button variant="contained" type="submit" sx={{ m: 2 }}>
+                Generate
+            </Button>
         </form>
-    )
-}
+    );
+};
 
 export default Form;
