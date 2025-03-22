068f7e431fed72a0a5afc0dfbf6678e3f328ca08
Mark <mark.obrien78@btinternet.com>
2025-03-22 09:16:56 +0000

    Add clear icon

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,19 @@
 import { Button, TextField } from '@mui/material';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { Inputs, FormComponentProps } from '../types';
+import ClearIcon from '@mui/icons-material/Clear';
+import InputAdornment from '@mui/material/InputAdornment';
 
 const Form = ({ onFormSubmit }: FormComponentProps) => {
-    const { register, handleSubmit } = useForm<Inputs>();
+    const { register, handleSubmit, setValue } = useForm<Inputs>();
 
     const onSubmit: SubmitHandler<Inputs> = (data) => onFormSubmit(data);
+
+    const clearField = (
+        field: 'persona' | 'context' | 'task' | 'output' | 'constraint'
+    ) => {
+        setValue(field, '');
+    };
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
@@ -17,6 +25,18 @@ const Form = ({ onFormSubmit }: FormComponentProps) => {
                 sx={{ m: 2 }}
                 multiline
                 rows={5}
+                slotProps={{
+                    input: {
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <ClearIcon
+                                    onClick={() => clearField('persona')}
+                                    className="clear-icon"
+                                />
+                            </InputAdornment>
+                        ),
+                    },
+                }}
             />
             <TextField
                 id="context"
@@ -26,6 +46,17 @@ const Form = ({ onFormSubmit }: FormComponentProps) => {
                 sx={{ m: 2 }}
                 multiline
                 rows={5}
+                slotProps={{
+                    input: {
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <ClearIcon
+                                    onClick={() => clearField('context')}
+                                />
+                            </InputAdornment>
+                        ),
+                    },
+                }}
             />
             <TextField
                 id="task"
@@ -35,6 +66,15 @@ const Form = ({ onFormSubmit }: FormComponentProps) => {
                 sx={{ m: 2 }}
                 multiline
                 rows={5}
+                slotProps={{
+                    input: {
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <ClearIcon onClick={() => clearField('task')} />
+                            </InputAdornment>
+                        ),
+                    },
+                }}
             />
             <TextField
                 id="output"
@@ -44,6 +84,17 @@ const Form = ({ onFormSubmit }: FormComponentProps) => {
                 sx={{ m: 2 }}
                 multiline
                 rows={5}
+                slotProps={{
+                    input: {
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <ClearIcon
+                                    onClick={() => clearField('output')}
+                                />
+                            </InputAdornment>
+                        ),
+                    },
+                }}
             />
             <TextField
                 id="constraint"
@@ -53,6 +104,17 @@ const Form = ({ onFormSubmit }: FormComponentProps) => {
                 sx={{ m: 2 }}
                 multiline
                 rows={5}
+                slotProps={{
+                    input: {
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <ClearIcon
+                                    onClick={() => clearField('constraint')}
+                                />
+                            </InputAdornment>
+                        ),
+                    },
+                }}
             />
             <Button variant="contained" type="submit" sx={{ m: 2 }}>
                 Generate
