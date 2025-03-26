a030328a3bd8aec99d60743c409db9fd1005182b
Niamh Brown <niamh.brown@vet-ct.com>
2025-03-26 09:03:09 +0000

    removed comment.

--- src/types.ts
+++ src/types.ts
@@ -3,13 +3,6 @@ import { inputFormSchema } from './assets/inputFormSchema';
 
 export type Inputs = z.infer<typeof inputFormSchema>;
 
-// export type Inputs = {
-//     persona: string;
-//     context: string;
-//     task: string;
-//     output: string;
-//     constraint: string;
-// };
 export type FormField = {
     name: 'persona' | 'context' | 'task' | 'output' | 'constraint';
     description: string;
