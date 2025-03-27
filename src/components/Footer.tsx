2e77c6716564b7883d8064ddf77fd9f75ae1b1ed
Mark <mark.obrien78@btinternet.com>
2025-03-27 19:04:47 +0000

    Style footer from the designs, mobile first

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
 import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid2';
 import Link from '@mui/material/Link';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const teamMembers: Array<string> = [
     'Adil Rahman',
@@ -16,40 +18,148 @@ const teamMembers: Array<string> = [
 
 const Footer = () => {
     return (
-        <Container component="footer">
+        <Container
+            component="footer"
+            sx={{
+                backgroundColor: '#12141D',
+                margin: 0,
+                color: '#FFFFFF',
+                height: '280px',
+                padding: '28px 28px 10px 28px',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+                fontFamily: 'Poppins',
+            }}
+        >
             <Container
                 id="team-container"
                 maxWidth="xs"
+                disableGutters
                 sx={{
-                    marginLeft: '0',
-                    paddingLeft: '0',
+                    margin: '0',
+                    padding: '0',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'flex-start',
+                    '@media (min-width: 0px)': {
+                        width: '100%',
+                        maxWidth: '100%',
+                    },
                 }}
             >
-                <Grid
-                    container
-                    spacing={2}
+                <Container disableGutters>
+                    <Box
+                        component="img"
+                        src="./src/assets/logo.png"
+                        alt="Five star api logo"
+                        sx={{
+                            width: '95px',
+                            height: '32px',
+                            position: 'relative',
+                            left: '-7px',
+                            marginBottom: '0.5em',
+                        }}
+                    />
+                    <Grid
+                        container
+                        spacing={1}
+                        sx={{
+                            textAlign: 'left',
+                            maxWidth: '200px',
+                        }}
+                    >
+                        <Grid size={12}>
+                            <Typography
+                                sx={{
+                                    fontSize: '10px',
+                                    fontWeight: 'bold',
+                                    fontFamily: 'Poppins',
+                                    marginBottom: '0.5em',
+                                }}
+                            >
+                                Team
+                            </Typography>
+                        </Grid>
+                        {teamMembers.map((member) => {
+                            return (
+                                <Grid size={6}>
+                                    <Typography
+                                        sx={{
+                                            fontSize: '10px',
+                                            fontFamily: 'Poppins',
+                                            color: '#CBC9C9',
+                                        }}
+                                    >
+                                        {member}
+                                    </Typography>
+                                </Grid>
+                            );
+                        })}
+                    </Grid>
+                </Container>
+                <Container
+                    disableGutters
                     sx={{
-                        textAlign: 'left',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        gap: '0.1em',
+                        '@media (min-width: 0px)': {
+                            maxWidth: '135px',
+                        },
                     }}
                 >
-                    <Grid size={12}>
-                        <Typography>Team members</Typography>
-                    </Grid>
-                    {teamMembers.map((member) => {
-                        return (
-                            <Grid size={6}>
-                                <Typography>{member}</Typography>
-                            </Grid>
-                        );
-                    })}
-                </Grid>
+                    <Box
+                        component="img"
+                        src="./src/assets/chingu-logo.png"
+                        alt="Chingu logo"
+                        sx={{
+                            width: '30px',
+                            height: '30px',
+                            marginBottom: '1em',
+                        }}
+                    />
+                    <Typography
+                        sx={{
+                            fontSize: '8px',
+                            color: '#CBC9C9',
+                        }}
+                    >
+                        This project was built by team 21
+                    </Typography>
+                    <Typography
+                        sx={{
+                            fontSize: '8px',
+                            color: '#CBC9C9',
+                        }}
+                    >
+                        as part of the Chingu Voyage 54
+                    </Typography>
+                </Container>
             </Container>
-            <Typography component="p">
-                All rights reserved &copy; 2025 Chingu
-            </Typography>
-            <Link href="https://github.com/chingu-voyages/V54-tier2-team-21">
-                <Typography>Github repo</Typography>
-            </Link>
+            <Container
+                sx={{
+                    textAlign: 'center',
+                }}
+            >
+                <Link href="https://github.com/chingu-voyages/V54-tier2-team-21">
+                    <Typography sx={{ fontSize: '8px', color: '#FFFFFF' }}>
+                        <GitHubIcon sx={{ cursor: 'pointer' }} />
+                    </Typography>
+                </Link>
+
+                <Typography
+                    component="p"
+                    sx={{
+                        fontSize: '8px',
+                        marginTop: '0.5em',
+                        fontFamily: 'Poppins',
+                    }}
+                >
+                    &copy; 2025 Chingu. All rights reserved.
+                </Typography>
+            </Container>
         </Container>
     );
 };
