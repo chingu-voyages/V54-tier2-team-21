0e5ca61d9cbf5b4666a76ac895f15650538c736d
Mark <mark.obrien78@btinternet.com>
2025-03-29 12:54:51 +0000

    Put styles into own file to make them reusable

---
+++ src/styles.ts
@@ -0,0 +1,14 @@
+export const styles = {
+    typography: {
+        fontSizeExtraSmall: '8px',
+        fontSizeSmall: '10px',
+    },
+    colors: {
+        footerBackground: '#12141D',
+        fontPrimary: '#FFFFFF',
+        fontSecondary: '#CBC9C9',
+        buttonBackground: 'linear-gradient(to right, #8A00F9 0%, #FD99FF 100%)',
+        containerBackgroundWithBorder:
+            'linear-gradient(#313342, #313342) padding-box, linear-gradient(to right, #8A00F9 0%, #FD99FF 100%) border-box',
+    },
+};
