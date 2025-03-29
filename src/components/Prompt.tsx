e3159f0b929322200f5e41797c0418ac504a9391
Mark <mark.obrien78@btinternet.com>
2025-03-29 09:33:50 +0000

    Implement designs to prompt

--- src/components/Prompt.tsx
+++ src/components/Prompt.tsx
@@ -1,5 +1,5 @@
-import { Button } from '@mui/material';
-
+import { Button, Typography } from '@mui/material';
+import Box from '@mui/material/Box';
 interface PromptProps {
     prompt: string;
     onPromptSubmit: (prompt: string) => void;
@@ -8,15 +8,42 @@ interface PromptProps {
 const Prompt = ({ prompt, onPromptSubmit }: PromptProps) => {
     return (
         <>
-            <p>{prompt}</p>
-            <Button
-                variant="contained"
-                type="submit"
-                sx={{ m: 2 }}
-                onClick={() => onPromptSubmit(prompt)}
+            <Box
+                component="section"
+                sx={{
+                    width: '100%',
+                    minHeight: '250px',
+                    border: '3px solid transparent',
+                    background:
+                        'linear-gradient(#313342, #313342) padding-box, linear-gradient(to right, #8A00F9 0%, #FD99FF 100%) border-box',
+                    borderRadius: '8px',
+                    marginBottom: '4em',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                }}
             >
-                Submit
-            </Button>
+                <Typography sx={{ m: 2, color: '#FFFFFF' }}>
+                    {prompt ? prompt : 'PLEASE ENTER A PROMPT'}
+                </Typography>
+                <Button
+                    variant="contained"
+                    type="submit"
+                    sx={{
+                        m: 4,
+                        padding: '0.5em 2em',
+                        fontSize: '1rem',
+                        fontWeight: 600,
+                        background:
+                            'linear-gradient(to right, #8A00F9 0%, #FD99FF 100%)',
+                        alignSelf: 'center',
+                        borderRadius: '15px',
+                    }}
+                    onClick={() => onPromptSubmit(prompt)}
+                >
+                    Submit
+                </Button>
+            </Box>
         </>
     );
 };
