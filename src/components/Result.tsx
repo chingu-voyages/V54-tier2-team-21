ef7595000701a499b1bdb49be2de7576f0e1da77
Mark <mark.obrien78@btinternet.com>
2025-03-30 10:13:32 +0100

    Remove redundant code

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,13 +1,10 @@
 import { Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import { styles } from '../styles';
-import { useTheme } from '@mui/material/styles';
 interface ResultProps {
     result: string;
 }
 const Result = ({ result }: ResultProps) => {
-    const theme = useTheme();
-
     return (
         <Box
             component="section"
