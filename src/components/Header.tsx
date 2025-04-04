3a4ffd6b21dbad0ada2ea7af70f9bf3cd08b9575
Mark <mark.obrien78@btinternet.com>
2025-04-04 18:34:21 +0100

    Update to header from the designs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,32 @@
 import { Typography } from '@mui/material';
 import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import { styles } from '../styles';
 
 const Header = () => {
     return (
-        <Container component="header">
-            <Typography variant="h1">5 STAR AI</Typography>
+        <Container
+            component="header"
+            sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                padding: '0.75em',
+                borderBottom: '1px solid #3A3737',
+            }}
+        >
+            <Box
+                component="img"
+                src="/logo.png"
+                alt="Five star api logo"
+                sx={{
+                    width: '95px',
+                    height: '32px',
+                }}
+            />
+            <Typography sx={{ fontSize: styles.typography.fontSizeSmall }}>
+                April 2025
+            </Typography>
         </Container>
     );
 };
