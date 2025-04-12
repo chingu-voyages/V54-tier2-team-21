2fe712a0f2ed5395fdc8880e4e6c8d10c08c9831
Mark <mark.obrien78@btinternet.com>
2025-04-12 19:02:02 +0100

    Dummy dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,18 @@
+import { Box, Typography } from '@mui/material';
+
 const Dashboard = () => {
-    return <h4>You are now in the dashboard</h4>;
+    return (
+        <Box
+            sx={{
+                height: '400px',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+        >
+            <Typography variant="h4">You are now in the dashboard</Typography>
+        </Box>
+    );
 };
 
 export default Dashboard;
